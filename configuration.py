8c8cf6ef73643ba1e639ed3aa4b5f9f4c4cb5aa7
Abigail Reyes <aby.11jm@gmail.com>
2025-04-22 19:05:14 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-d6a7eeed-53ba-4047-95cd-9305aea7865f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
